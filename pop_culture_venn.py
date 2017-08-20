f282120d8148ae55b4925f0fdeaca75656211092
phillipcrout <phillip_crout@hotmail.co.uk>
2017-08-20 11:39:49 +0100

    final tidy up

--- pop_culture_venn.py
+++ pop_culture_venn.py
@@ -3,10 +3,10 @@ import numpy as np
 from matplotlib_venn import *
 import pandas as pd
 
-T = pd.Series(['Alcohol','Parent Child','Man','Sidekick','Subsiduary'])
-A = pd.Series([1,1,1,0,1])
-R   = pd.Series([1,1,1,1,1])
-B = pd.Series([1,1,1,1,0])
+T = pd.Series(['Alcohol','Parent Child','Sidekick','Subsiduary','Sex'])
+A = pd.Series([1,1,0,1,1])
+R   = pd.Series([1,1,1,1,0])
+B = pd.Series([1,1,1,0,1])
 
 df = pd.DataFrame({'theme':T,'Archer':A,'Rick':R,'Bojack':B})
 
@@ -16,9 +16,7 @@ C = set(df.theme[df.Bojack==1])  #BoJack
 
 v = venn3_unweighted([A,B,C], ('Archer', 'R&M', 'BoJack'))
 
-#v.get_label_by_id('100').set_text('\n'.join(A-B-C))
 v.get_label_by_id('110').set_text('\n'.join(A&B-C))
 v.get_label_by_id('011').set_text('\n'.join(B&C-A))
 v.get_label_by_id('101').set_text('\n'.join(A&C-B))
 v.get_label_by_id('111').set_text('\n'.join(A&B&C))
-#v.get_label_by_id('011').set_text('\n'.join(B&C-A))
