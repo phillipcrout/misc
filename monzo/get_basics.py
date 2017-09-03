d3d82e6d34f46ee6c0b8f62d6c9e8edc994c2893
phillipcrout <phillip_crout@hotmail.co.uk>
2017-09-03 17:16:42 +0200

    polish

--- monzo/get_basics.py
+++ monzo/get_basics.py
@@ -1,30 +1,24 @@
-from monzo.monzo import Monzo # Import Monzo Class
+from monzo.monzo import Monzo # Add link to source
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns #for the nicer default styles
-
 from secret_string import *
 
-client = Monzo(secret_string) # Replace access token with a valid token found at: https://developers.getmondo.co.uk/
-account_id = client.get_first_account()['id'] # Get the ID of the first account linked to the access token
+def clean_df_for_pie(df):
+    df = df[df.amount<0] #only take outgoings
+    df  = df.groupby('category').sum() #reduce to cat type
+    df = df*-1 #change sign
+    return df
 
-"""
-## Demo 
-balance = client.get_balance(account_id) # Get your balance object
-print(balance['balance']) # 100000000000
-print(balance['currency']) # GBP
-print(balance['spend_today']) # 2000
-"""
-
+## get data off Monzo
+client = Monzo(secret_string) 
+account_id = client.get_first_account()['id'] 
 transactions = client.get_transactions(account_id)['transactions']
+
+## convert the interesting subset into a df
 col = ['amount','include_in_spending','notes','category']
 df = pd.DataFrame(data=transactions)[col]
 
-def clean_for_pie(df):
-    df_neg = df[df.amount<0]
-    df_gr  = df_neg.groupby('category').sum()
-    df_gr = df_gr*-1
-    return df_gr
-    
-dfx = clean_for_pie(df)
-plt.pie(dfx.amount[dfx.index!='entertainment'],labels=dfx.index[dfx.index!='entertainment'])
+dfx = clean_df_for_pie(df)
+dfx = dfx[dfx.index!='entertainment'] # dominate catergory
+plt.pie(dfx.amount,labels=dfx.index)
