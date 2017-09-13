5e331ee9baee6c0f2990c29fdcf5a0ae0803d02d
phillipcrout <phillip_crout@hotmail.co.uk>
2017-09-13 21:18:41 +0200

    getting something like an array going

--- Ising/C++/hello_world.cpp
+++ Ising/C++/hello_world.cpp
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <iostream>
+using namespace std;
+
+const int size = 3; //this is a constant so that the lattice can be created.
+int lattice [size];
+
 int main ()
 {
-printf("Hello World!\n");
+cout << size << endl;
+cout << lattice[2] << endl;
 }
