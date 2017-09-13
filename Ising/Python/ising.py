ee3d69e457ab6c6c04b5d2822158f63a83f0c2f6
phillipcrout <phillip_crout@hotmail.co.uk>
2017-09-13 18:03:09 +0200

    basic pseudo code

--- Ising/Python/ising.py
+++ Ising/Python/ising.py
@@ -1,5 +1,19 @@
 import numpy as np
+# from numba import jit
+
+def calculate_local_energy(landscape):
+    pass
+
+def single_timestep(landscape,T):
+   ## generate two random numbers 
+   ## call energy function
+   ## generate random number for Boltzmann
+   ## flip?
+   return landscape,T
+
 
 size = 100
-
 ising_landscape = np.zeros([size,size]) 
+
+for _ in range(size**2):
+    landscape,T = single_timestep(landscape,T)
