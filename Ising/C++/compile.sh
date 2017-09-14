69a9665764c921b0096fa5133bf7a03a204ceee4
phillipcrout <phillip_crout@hotmail.co.uk>
2017-09-14 13:50:30 +0200

    added compile shell and vector template

---
+++ Ising/C++/compile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+### for easier compile
+
+g++ $1.cpp -o ex_$1
--- Ising/C++/hello_world.cpp
+++ Ising/C++/hello_world.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 const int size = 3; //this is a constant so that the lattice can be created.
@@ -9,4 +11,25 @@ int main ()
 {
 cout << size << endl;
 cout << lattice[2] << endl;
+
+vector< vector<int> > a;
+
+//m * n is the size of the matrix
+
+int m = 2, n = 4;
+//Grow rows by m
+a.resize(m);
+for(int i = 0 ; i < m ; ++i)
+   {
+   //Grow Columns by n
+   a[i].resize(n);
+   }
+//Now you have matrix m*n with default values
+
+//you can use the Matrix, now
+a[1][0]=1;
+a[1][1]=2;
+a[1][2]=3;
+a[1][3]=4;
+cout << a[1][1] << endl;
 }
